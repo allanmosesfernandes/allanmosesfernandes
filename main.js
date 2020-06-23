5cac4fe3278dfd2752032f94dc2472a96d0cf263
Allan Fernandes <fernzallan14@gmail.coms>
2020-06-23 12:35:07 +0530

    build-migration-Netlify

--- main.js
+++ main.js
@@ -102,3 +102,49 @@ function songSlide(e) {
 
 // audio.addEventListener('ended', nextSong);
 
+let select = s => document.querySelector(s),
+  selectAll = s =>  document.querySelectorAll(s),
+		mainSVG = select('#mainSVG'),
+		pacman = select('#pacman'),
+		allFood = selectAll('#food *')
+
+gsap.set('svg', {
+	visibility: 'visible'
+})
+gsap.set('circle', {
+	transformOrigin: '50% 50%'
+})
+
+let tl = gsap.timeline();
+tl.to(pacman, {
+	duration: 0.25,
+	drawSVG:'90% 10%',
+	x: 50,
+	ease: 'sine.inOut',
+	repeat: -1,
+	yoyo: true
+})
+.to('#startFood', {
+	duration: 0.5,
+	repeat: -1,
+	x: '-=100',
+	scale: 0,
+	ease: 'sine.inOut'
+}, 0)
+.to(allFood, {
+	duration: 0.25,
+	x: '-=50',
+	stagger: {
+		each: 0.05
+	},
+		ease: 'sine.inOut',
+		repeat: -1
+}, 0)
+.from(allFood[5], {
+	duration: 0.5,
+	repeat: -1,
+	scale: 0
+}, 0)
+
+//tl.timeScale(0.25)
+//ScrubGSAPTimeline(tl)
