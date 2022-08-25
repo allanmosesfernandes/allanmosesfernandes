fb1bd742296355b2dc09fce9ce27a4d9c8d3da28
Allan Fernandes <Fernzallan14@gmail.com>
2022-08-25 13:39:40 +0100

    init+html

---
+++ REACT_STUFF/crwnclothing2022/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+const App = () => {
+
+const categories = [
+  {
+    id: 1,
+    title: 'Hats'
+  },
+  {
+    id: 2,
+    title: 'Jackets'
+  },
+  {
+    id: 3,
+    title: 'Sneakers'
+  },
+  {
+    id: 4,
+    title: 'Women'
+  },
+  {
+    id: 5,
+    title: 'Men'
+  }
+]
+
+
+
+
+  return (
+    <div className="categories-container">
+      <h1>Ankan</h1>
+    </div>
+  );
+}
+
+export default App;
